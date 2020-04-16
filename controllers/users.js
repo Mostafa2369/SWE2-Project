56e2c1a789f1a8a08781232942e8de11a7cb4c66
Mostafa2369 <mostafa2369@gmail.com>
2020-04-16 22:26:31 +0200

    modify user controller

--- controllers/users.js
+++ controllers/users.js
@@ -8,13 +8,14 @@ const User = require('../models/User');
 // @route     POST /api/v1/users/auth/register
 // @access    Public
 exports.register = asyncHandler(async (req, res, next) => {
-    const { name, email, password } = req.body;
+    const { name, email, password , role } = req.body;
   
     // Create user
     const user = await User.create({
-        name: name,
-       email : email,
-      password:password,
+        name,
+       email,
+      password,
+      role
       
     });
   
@@ -22,33 +23,62 @@ exports.register = asyncHandler(async (req, res, next) => {
   });
   
 
+
+// @desc      Login user
+// @route     POST /api/v1/users/auth/login
+// @access    Public
+exports.login = asyncHandler(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  // Validate emil & password
+  if (!email || !password) {
+    return next(new ErrorResponse('Please provide an email and password', 400));
+  }
+
+  // Check for user
+  const user = await User.findOne({ email }).select('password');
   
+  
+
+  if (!user) {
+    return next(new ErrorResponse('Invalid credentials', 401));
+  }
+
+  // Check if password matches
+  const isMatch = await user.matchPassword(password);
+
+  if (!isMatch) {
+    return next(new ErrorResponse('Invalid credentials', 401));
+  }
+
+  sendTokenResponse(user, 200, res);
+});
+
 // Get token from model, create cookie and send response
 const sendTokenResponse = (user, statusCode, res) => {
-    // Create token
-    const token = user.getSignedJwtToken();
-  
-    const options = {
-      expires: new Date(
-        Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000
-      ),
-      httpOnly: true
-    };
-  
-  
-    res
-      .status(statusCode)
-      .cookie('token', token, options)
-      .json({
-        success: true,
-        token
-      });
+  // Create token
+  const token = user.getSignedJwtToken();
+
+  const options = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true
   };
+
+
+  res
+    .status(statusCode)
+    .cookie('token', token, options)
+    .json({
+      success: true,
+      token
+    });
+};
   
 // @desc      Get all users
 // @route     GET /api/v1/users
 // @access    Puplic
 exports.getUsers = asyncHandler(async (req, res, next) => {
-    res.status(200).json(res.advancedResults);
-  });
-  
+  res.status(200).json(res.advancedResults);
+});
